83eb4cbd56cff3a9ef13462a9f36c5fd90f33fcf
Dhairya Upadhyay <dhairya221b@gmail.com>
2025-11-26 04:58:47 +0530

    ✨ update version to 2.1.0 and optimize objectsDiff function for better performance

--- packages/runtime/src/utils/objects.js
+++ packages/runtime/src/utils/objects.js
@@ -1,12 +1,20 @@
 export function objectsDiff(oldObj, newObj) {
   const oldKeys = Object.keys(oldObj);
   const newKeys = Object.keys(newObj);
-  // TODO: Optimize this function to avoid multiple iterations
+  const added = [];
+  const updated = [];
+
+   newKeys.forEach(key => {
+    if (!(key in oldObj)) {
+      added.push(key);
+    }    
+    if (key in oldObj && oldObj[key] !== newObj[key]) {
+      updated.push(key);
+    }
+  });
   return {
-    added: newKeys.filter((key) => !(key in oldObj)),
+    added,
     removed: oldKeys.filter((key) => !(key in newObj)),
-    updated: newKeys.filter(
-      (key) => key in oldObj && oldObj[key] !== newObj[key]
-    ),
+    updated,
   };
 }
