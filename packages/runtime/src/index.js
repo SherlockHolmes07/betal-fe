0b1083f41fb19a93cbe7c054211a81c9ff4c7546
Dhairya Upadhyay <dhairya221b@gmail.com>
2025-12-20 23:17:22 +0530

    :sparkles: :technologist: add nextTick function to scheduler for testing async fns

--- packages/runtime/src/index.js
+++ packages/runtime/src/index.js
@@ -1,3 +1,4 @@
 export { createBetalApp } from './app.js';
 export { defineComponent } from './component.js';
 export { h, hFragment, hString } from './h.js';
+export { nextTick } from './scheduler.js';
